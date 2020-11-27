54de81b607ad6098e30305f2404ae5bb1b159d52
bm16ton <bm16ton@gmail.com>
2020-11-26 19:01:01 -0500

    add icon stuff

--- install.sh
+++ install.sh
@@ -6,7 +6,8 @@ cp syngui /usr/local/bin/
 chmod +x /usr/share/syngui/synlist
 chmod +x /usr/share/syngui/synvar
 chmod +x /usr/local/bin/syngui
-cp syngui.png /usr/share/icons/
+cp syngui.png /usr/share/icons/hicolor/scalable/apps/
+gtk-update-icon-cache -f -i /usr/share/icons/hicolor/
 cp syngui.desktop /usr/share/applications/
 
 
